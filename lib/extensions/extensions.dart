615aa814faee88bc5a7db93100c34a8d63ba67bd
muhammadmateen027 <mateenumt027@gmail.com>
2023-02-23 11:46:18 +0100

    Update extensions

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -109,3 +109,19 @@ extension BuildContextExtension on BuildContext {
 
   bool get isDesktop => screenWidth >= 1024;
 }
+
+extension DateTimeExension on DateTime {
+  bool isSameDay(final DateTime other) =>
+      day == other.day && month == other.month && year == other.year;
+
+  bool isLaterOrEqualDay(final DateTime other) =>
+      isSameDay(other) || isAfter(other);
+
+  DateTime get firstDayOfMonth => DateTime(year, month);
+
+  DateTime get firstDayOfNextMonth => DateTime(year, month + 1);
+
+  DateTime get secondDayOfNextMonth => DateTime(year, month + 1, 2);
+
+  DateTime get lastDayOfPreviousMonth => DateTime(year, month, 0);
+}
