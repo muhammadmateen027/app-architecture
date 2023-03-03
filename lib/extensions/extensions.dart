79cc587140b5196e0c30f9b784ab47f51f6145f6
muhammadmateen027 <mateenumt027@gmail.com>
2023-03-03 09:11:12 +0100

    Update the extension

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -78,6 +78,15 @@ extension LiftGenericExtensions<T extends Object> on T? {
 
 extension ListExtension<T> on List<T> {
   bool hasExact(final int value) => length == value;
+
+  T? elementAtOrNull(final int index) {
+    try {
+      return elementAt(index);
+      // ignore: avoid_catching_errors
+    } on RangeError {
+      return null;
+    }
+  }
 }
 
 extension JsonConverterExtension on dynamic {
