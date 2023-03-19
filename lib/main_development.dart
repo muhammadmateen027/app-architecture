4c868749a7d05f736b7a8d02393e7ee42702d0f8
muhammadmateen027 <mateenumt027@gmail.com>
2023-03-19 16:00:20 +0100

    Initialize the database and add comments what to do next

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,25 @@
+import 'dart:io';
+
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 import 'package:stars/app/app.dart';
 import 'package:stars/bootstrap.dart';
+import 'package:stars/persistance/database/database.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<File> get _location async {
+  final dbFolder = await getApplicationDocumentsDirectory();
+
+  return File(p.join(dbFolder.path, 'your_db_name'));
+}
+
+void main() async {
+  final database = Database(NativeDatabase(await _location));
+
+  // TODO(daoInitialization): Initialize your dao while passing the database.
+
+  // TODO(RepositoryInitialization): initialize your repository while passing
+  //  respective dao
+
+  await bootstrap(() => const App());
 }
