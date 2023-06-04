2002ee141d74a045f25ad23f02e48d84d23dbe43
muhammadmateen027 <muhammadmateen027@gmail.com>
2023-06-04 18:05:43 +0200

    Add license and update life cycle

--- lib/system/app_life_cycle/state_observer.dart
+++ lib/system/app_life_cycle/state_observer.dart
@@ -29,6 +29,7 @@ class _StateObserverState extends State<StateObserver>
         widget.onLifecycleChanged(LifecycleState.paused);
         break;
       case AppLifecycleState.inactive:
+      case AppLifecycleState.hidden:
       case AppLifecycleState.detached:
         widget.onLifecycleChanged(LifecycleState.background);
         break;
